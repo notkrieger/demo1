0af02387cf33c05bf5ff996c858d306dab11e164
Rhys Tyne <tynerhys@gmail.com>
2023-08-15 10:45:47 +1000

    chnaged partC to be more reliant on matrix operations

--- partC.py
+++ partC.py
@@ -2,30 +2,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import multiprocessing as mp
 
 depth = 250
-halfDepth = int(depth/2)
 step = 0.0005
 start = 2.8
 end = 4
 length = int((end - start)/step)
-rs = torch.arange(start, end, step) # growth rate
+rs = torch.arange(start, end, step) # constants
 
-#zs = torch.zeros((length, halfDepth)) # for integer start/end values
-zs = torch.zeros((length + 1, halfDepth)) # for decimal start/end values
+zs = torch.ones(length + 1) * 0.3 # set start value to arbitrary number between 0 and 1
 
-for j in range(length):
-    r = rs[j] # define growth constant
-    x_ = 0.3  # arbitrary initial value
+# mulitprocessing for parallelism??
+numProcesses = 10
+pool = mp.Pool(numProcesses)
+gs = 8
 
-    xs = torch.zeros(depth)
-    xs[0] = x_
+def run(zs, rs, depth):
+    for i in range(depth - 1): # got through depth
+        zs_ = zs * rs * (1 - zs) # find next iteration
+        zs = zs_ # redefine zs
+        if i > 3*depth/4: # need to plot all "stable" points
+            # without this step only one point for each r value is measured
+            plt.plot(rs, zs, 'ko', ms = 0.025)
 
-    for i in range(1, depth):
-        xs[i] = xs[i-1] * r * (1 - xs[i-1])
-        if i > halfDepth:
-            zs[j][i - halfDepth] = xs[i]
-
-plt.plot(rs, zs, 'kx', ms = 0.005)
+run(zs, rs, depth)
 plt.show()
-
